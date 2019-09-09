3182f88c8e63472c4b65d60be03b6ec996153c39
Jackz <me@jackz.me>
2019-09-09 16:36:49 -0500

    prevent server switch of A to A

--- src/main/java/me/jackz/simplebungee/events/PlayerEvents.java
+++ src/main/java/me/jackz/simplebungee/events/PlayerEvents.java
@@ -26,7 +26,8 @@ public class PlayerEvents implements Listener {
     private PlayerLoader playerLoader;
 
     private boolean network_messages;
-    private boolean switch_server;
+    private boolean switch_server_messages;
+    private boolean friends_messages; //move friends methods outside to lib, then get friends list for event player, and do loop to send message
 
     public PlayerEvents(SimpleBungee plugin) {
         this.plugin = plugin;
@@ -35,10 +36,12 @@ public class PlayerEvents implements Listener {
         try {
             config = plugin.getConfig();
             network_messages = config.getBoolean("connection-messages.bungee");
-            switch_server = config.getBoolean("connection-messages.serverswitch");
+            switch_server_messages = config.getBoolean("connection-messages.serverswitch");
+            friends_messages = config.getBoolean("connection-messages.friends");
         } catch (IOException e) {
             network_messages = false;
-            switch_server = false;
+            switch_server_messages = false;
+            friends_messages = true;
             plugin.getLogger().warning("Could not load config.yml, join messages disconnected");
         }
     }
@@ -79,7 +82,7 @@ public class PlayerEvents implements Listener {
     public void onServerConnect(ServerConnectEvent e) {
         ProxiedPlayer player = e.getPlayer();
         if(player.isConnected() && player.getServer() != null && !e.isCancelled()) {
-            if(switch_server) {
+            if(switch_server_messages) {
                 LAST_SERVER_MAP.put(player.getUniqueId(),e.getPlayer().getServer().getInfo());
             }
         }
@@ -88,10 +91,10 @@ public class PlayerEvents implements Listener {
     @EventHandler
     public void onServerSwitch(ServerSwitchEvent e) {
         ProxiedPlayer player = e.getPlayer();
-        if(switch_server) {
+        if(switch_server_messages) {
             ServerInfo previous = LAST_SERVER_MAP.get(player.getUniqueId());
             ServerInfo current = player.getServer().getInfo();
-            if(current != null) {
+            if(current != null && current != previous) {
                 TextComponent tc = new TextComponent(player.getName() + " switched servers from " + previous.getName() + " to " + current.getName());
                 tc.setColor(ChatColor.YELLOW);
                 plugin.getProxy().broadcast(tc);
