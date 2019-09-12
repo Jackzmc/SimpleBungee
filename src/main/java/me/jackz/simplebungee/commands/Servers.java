614fa412d9436906ce6ab8785934e9768e417500
Jackz <me@jackz.me>
2019-09-11 19:47:19 -0500

    mirrored /glist format for servers (but w/ joinable servers clickevent)

--- src/main/java/me/jackz/simplebungee/commands/Servers.java
+++ src/main/java/me/jackz/simplebungee/commands/Servers.java
@@ -1,16 +1,17 @@
 package me.jackz.simplebungee.commands;
 
 import me.jackz.simplebungee.SimpleBungee;
-import net.md_5.bungee.api.Callback;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
-import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+
+import java.util.stream.Collectors;
 
 public class Servers extends Command {
     private SimpleBungee plugin;
@@ -19,7 +20,7 @@ public class Servers extends Command {
         super("servers");
     }
     public Servers(SimpleBungee plugin)  {
-        super("servers");
+        super("servers","simplebungee.command.servers");
         this.plugin = plugin;
     }
     @Override
@@ -27,10 +28,10 @@ public class Servers extends Command {
         TextComponent base = new TextComponent("§6Server Status: ");
         for (ServerInfo server : plugin.getProxy().getServers().values()) {
             String name = server.getName();
-            String finalString = String.format("§e%d online",server.getPlayers().size());
-            TextComponent comp_server = new TextComponent("\n" + name +": ");
+            TextComponent comp_server = new TextComponent(String.format("\n%s[%s]",ChatColor.GREEN,name));
+            comp_server.setColor(ChatColor.GREEN);
             if(server.isRestricted()) {
-                comp_server.addExtra(" [no access]");
+                comp_server.addExtra(" [restricted]");
                 comp_server.setColor(ChatColor.RED);
             }else {
                 ClickEvent ce = new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/server " + name);
@@ -38,11 +39,10 @@ public class Servers extends Command {
                 //server_name.setHoverEvent(he);
                 comp_server.setHoverEvent(he);
                 comp_server.setClickEvent(ce);
-                comp_server.setColor(ChatColor.GRAY);
             }
-
+            String players = server.getPlayers().stream().map(ProxiedPlayer::getName).collect(Collectors.joining(", "));
+            String finalString = String.format(" %s(%d)%s: %s",ChatColor.YELLOW,server.getPlayers().size(),ChatColor.WHITE,players);
             TextComponent comp_count = new TextComponent(finalString);
-
             base.addExtra(comp_server);
             base.addExtra(comp_count);
         }
