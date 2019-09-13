04afe9d905280fbc043509fa5d7c5f11aa4dd61f
Jackz <me@jackz.me>
2019-09-13 07:27:27 -0500

    actually implemented server shortcut correctly - fixed the issue where it wasn't loading any aliases

--- src/main/java/me/jackz/simplebungee/lib/ServerShortcut.java
+++ src/main/java/me/jackz/simplebungee/lib/ServerShortcut.java
@@ -9,36 +9,48 @@ import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.config.Configuration;
 
 import java.util.List;
+import java.util.Map;
 
 public class ServerShortcut {
     public static void setupShortcuts(SimpleBungee plugin, Configuration servers) {
+        Map<String,ServerInfo> SERVERS = plugin.getProxy().getServers();
         if(servers != null) {
             for (String key : servers.getKeys()) {
                 if(key.equals("example")) continue; //ignore 'example'
-                boolean use_perms = servers.getBoolean(Util.getSectioned("server_shortcuts",key,"permissions"),false);
-                List<String> aliases_list = servers.getStringList(Util.getSectioned("server_shortcuts",key,"aliases"));
+                boolean use_perms = servers.getBoolean(Util.getSectioned(key,"permissions"),false);
+                List<String> aliases_list = servers.getStringList(Util.getSectioned(key,"aliases"));
+
                 if(aliases_list.size() > 0) {
-                    ServerInfo server = plugin.getProxy().getServerInfo(key);
+                    ServerInfo server = null;
+                    for (ServerInfo value : SERVERS.values()) {
+                        if(value.getName().equalsIgnoreCase(key)) {
+                            server = value;
+                            break;
+                        }
+                    }
                     if(server == null) {
                         plugin.getLogger().warning("Server in config section 'server_shortcuts' does not exist: " + key);
                         continue;
                     }
                     String first = aliases_list.get(0);
-                    String[] aliases = aliases_list.subList(1,aliases_list.size()-1).toArray(new String[0]);
-
+                    String[] aliases = (aliases_list.size() >= 2) ? aliases_list.subList(1,aliases_list.size()).toArray(new String[0]) : new String[0];
                     String permissions = (use_perms) ? Util.getSectioned("simplebungee","server",key) : null;
 
-                    plugin.getProxy().getPluginManager().registerCommand(plugin, new Command(key,permissions,aliases)  {
+                    //plugin.getLogger().info("Registering aliases[" + aliases_list.size() + "] for '" + key + "'. first: " + first + " | rest: " + String.join(",",aliases));
+                    ServerInfo finalServer = server;
+                    plugin.getProxy().getPluginManager().registerCommand(plugin, new Command(first,permissions,aliases)  {
                         @Override
                         public void execute(CommandSender sender, String[] args) {
                             if(sender instanceof ProxiedPlayer) {
                                 ProxiedPlayer player = (ProxiedPlayer) sender;
-                                player.connect(server);
+                                player.connect(finalServer);
                             }else{
                                 sender.sendMessage(new TextComponent("§cYou must be a player to use this"));
                             }
                         }
                     });
+                }else{
+                    plugin.getLogger().warning("Server shortcut '" + key + "' has no aliases defined");
                 }
 
             }
